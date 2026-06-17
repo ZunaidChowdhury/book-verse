5552b12e80a978f0fa694cdd105c2f741b2ae98c
Zunaid <133950183+ZunaidChowdhury@users.noreply.github.com>
2026-06-17 23:01:15 +0600

    project setup done

---
+++ src/app/page.js
@@ -0,0 +1,18 @@
+import { Button } from "@heroui/react";
+import Image from "next/image";
+
+
+export default function Home() {
+  return (
+    <div>
+      <h1>Home</h1>
+
+
+      {/* <Image
+      src='/design.png'
+      width={1000}
+      height={5000}
+       /> */}
+    </div>
+  );
+}
